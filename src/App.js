2289a7c13a85bae14afb80af3485075fb7e3a240
Aditria Pardana <aditria.pardana20@gmail.com>
2022-09-13 16:20:53 +0700

    update axios api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Form, TextArea, Button, Icon } from "semantic-ui-react";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [inputText, setInputText] = useState("");
+  const [detectLanguageKey, setdetectedLanguageKey] = useState("");
+  const [selectedLanguageKey, setLanguageKey] = useState("");
+  const [languagesList, setLanguagesList] = useState([]);
+  const [resultText, setResultText] = useState("");
+
+  const getLanguageSource = () => {
+    axios
+      .post(`https://libretranslate.de/detect`, {
+        q: inputText,
+      })
+      .then((response) => {
+        setdetectedLanguageKey(response.data[0].language);
+      });
+  };
+
+  useEffect(() => {
+    axios.get(`https://libretranslate.de/languages`).then((response) => {
+      setLanguagesList(response.data);
+    });
+  }, []);
+
+  const languageKey = (selectedLanguage) => {
+    setLanguageKey(selectedLanguage.target.value);
+  };
+
+  const translateText = () => {
+    getLanguageSource();
+
+    let data = {
+      q: inputText,
+      source: detectLanguageKey,
+      target: selectedLanguageKey,
+    };
+
+    axios.post(`https://libretranslate.de/translate`, data).then((response) => {
+      setResultText(response.data.translatedText);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React JS
-        </a>
-      </header>
+    <div>
+      <div className="app-header">
+        <h2 className="header">iTranslate</h2>
+      </div>
+
+      <div className="app-body">
+        <div>
+          <Form>
+            <Form.Field
+              control={TextArea}
+              placeholder="Type Text to Translate.."
+              onChange={(e) => setInputText(e.target.value)}
+            />
+
+            <select className="language-select" onChange={languageKey}>
+              <option>Please Select Language..</option>
+              {languagesList.map((language) => {
+                return <option value={language.code}>{language.name}</option>;
+              })}
+            </select>
+            <br />
+
+            <Form.Field
+              control={TextArea}
+              placeholder="Your Result Translation.."
+              value={resultText}
+            />
+
+            <Button color="blue" size="large" onClick={translateText}>
+              <Icon name="translate" />
+              Translate
+            </Button>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
